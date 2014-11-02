8524d0319b57941d076a8b4d48749f1b7f23e437
mockenoff <mockenoff@yahoo.com>
2014-11-02 05:07:50 -0600

    Add dragging functionality

--- js/flow.js
+++ js/flow.js
@@ -17,7 +17,7 @@ function Flow(el) {
 
 	for (var i = 0; i < count; i++) {
 		slides[i].className += ' flow-slide';
-		slides[i].style.left = 'calc(50% - '+Math.floor(size.width / 2)+'px)';
+		slides[i].style.left = 'calc(50% - '+Math.round(size.width / 2)+'px)';
 	}
 
 	xform = 'transform';
@@ -30,7 +30,7 @@ function Flow(el) {
 		return true;
 	});
 
-	self.update = function(){
+	self.update = function(value){
 		var diff = 0,
 			slide = 0,
 			mlide = 0,
@@ -38,26 +38,88 @@ function Flow(el) {
 			translateZ = 0,
 			rotateY = 0;
 
+		if (value === undefined) {
+			value = current;
+		}
+
 		for (var i = 0; i < count; i++) {
-			diff = i * size.width - current;
+			diff = i * size.width - value;
 			slide = diff / total;
 			mlide = Math.abs(slide);
 
 			translateX = diff * 0.65;
-			translateZ = -800 * mlide;
-			rotateY = -90 * 50 * (slide / count);
+			translateZ = -1000 * mlide;
+			rotateY = -90 * 60 * (slide / count);
 
-			if (rotateY < -90) {
-				rotateY = -90;
-			} else if (rotateY > 90) {
-				rotateY = 90;
+			if (rotateY < -100) {
+				rotateY = -100;
+			} else if (rotateY > 100) {
+				rotateY = 100;
 			}
 
 			slides[i].style[xform] = 'translateX('+translateX+'px) translateZ('+translateZ+'px) rotateY('+rotateY+'deg)';
-			slides[i].style.zIndex = Math.floor(mlide * -count);
+			slides[i].style.zIndex = count - Math.round(mlide * count);
 			// slides[i].style.opacity = 1 - mlide;
 		}
 	};
+
+	var origin = {x: 0, y: 0},
+		pressed = false,
+		timestamp = null;
+
+	function xpos(e) {
+		if ('targetTouches' in e && e.targetTouches.length >= 1) {
+			return e.targetTouches[0].clientX;
+		}
+		return e.clientX;
+	}
+
+	function ypos(e) {
+		if ('targetTouches' in e && e.targetTouches.length >= 1) {
+			return e.targetTouches[0].clientY;
+		}
+		return e.clientY;
+	}
+
+	function tap(e) {
+		e.preventDefault();
+
+		if (pressed === false) {
+			console.log('tap', xpos(e), ypos(e));
+			pressed = true;
+			timestamp = Date.now();
+			origin = {x: xpos(e), y: ypos(e)};
+		}
+	}
+
+	function drag(e) {
+		e.preventDefault();
+
+		if (pressed === true) {
+			console.log('drag', xpos(e), origin.x, (xpos(e) - origin.x));
+			self.update(current + (origin.x - xpos(e)));
+		}
+	}
+
+	function release(e) {
+		e.preventDefault();
+
+		if (pressed === true) {
+			console.log('release', xpos(e), ypos(e));
+			pressed = false;
+			current += origin.x - xpos(e);
+		}
+	}
+
+	el.addEventListener('mousedown', tap);
+	document.body.addEventListener('mouseup', release);
+	document.body.addEventListener('mousemove', drag);
+
+	if (typeof window.ontouchstart !== 'undefined') {
+		el.addEventListener('touchstart', tap);
+		document.body.addEventListener('touchmove', drag);
+		document.body.addEventListener('touchend', release);
+	}
 
 	self.update();
 }
